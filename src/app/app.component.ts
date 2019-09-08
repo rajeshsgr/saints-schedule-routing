0fea3a01f17f97e6c05586f72f441b27120e89af
Rajesh P <rajeshp@Rajeshs-MacBook-Pro.local>
2019-09-07 20:22:07 -0500

    Navigating Programmatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'saintsTeamSchedule';
+
+  constructor(private router: Router) {
+
+  }
+
+  loadSchedulePage() {
+
+    this.router.navigate(['/match-schedule']);
+
+  }
+
+  loadRosterPage() {
+
+    this.router.navigate(['/team-roster']);
+
+  }
 }
